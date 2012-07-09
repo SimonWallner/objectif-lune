463da58c6e6d0133be6bf12979c81528af953d7e
Simon Wallner <me@simonwallner.at>
2012-07-09 16:22:30 +0200

    solved the missing include file stupidity.

--- server/src/objectifLune.cpp
+++ server/src/objectifLune.cpp
@@ -1,11 +1,18 @@
 #include <objectifLune.hpp>
 
-#include <websocketpp.hpp>
-
 using namespace objectifLune;
 
 void Server::hello()
 {
 	std::cout << "bonjour, lune!" << std::endl;
-	
 }
+
+Server::Server(int _portNumber)
+	: portNumber(_portNumber)
+{}
+
+//void Server::on_message(websocketpp::server::connection_ptr con,
+//						websocketpp::message::data_ptr msg) {
+//	con->send(msg->get_payload(), msg->get_opcode());
+//
+// }
--- server/src/objectifLune.hpp
+++ server/src/objectifLune.hpp
@@ -3,12 +3,23 @@
 
 #include <iostream>
 
+#include <websocketpp.hpp>
+
 namespace objectifLune
 {
-	class Server
+	class Server// : public websocketpp::server::handler
 	{
+	private:
+		int portNumber;
+		
 	public: 
+		
+		Server(int portNumber = 62000);
+
 		void hello();
+		
+//		void on_message(websocketpp::server::connection_ptr con,
+//						websocketpp::message::data_ptr msg);
 	};
 }
 
