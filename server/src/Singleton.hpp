3bb170299193a3d743e8391e8d826a5ac54bcbbf
Simon Wallner <me@simonwallner.at>
2012-08-24 10:04:44 +0200

    started to add a singleton accessor

---
+++ server/src/Singleton.hpp
@@ -0,0 +1,35 @@
+#ifndef OBJECTIF_LUNE_SINGLETON
+#define OBJECTIF_LUNE_SINGLETON
+
+#include "Server.hpp"
+
+namespace objectifLune
+{
+	/**
+	 * Simple singleton accessor to an objectif lune object.
+	 */
+	class Singleton
+	{
+	private:
+		static Server* instance;
+		Singleton() {}
+		Singleton(const Singleton&) {}
+		~Singleton() {}
+	public:
+		static Server& Get();
+	};
+	
+	Server* Singleton::instance = 0;
+	
+	Server& Singleton::Get()
+	{
+		if (!instance)
+		{
+			instance = new Server();
+			instance->startService();
+		}
+		return *instance;
+	}
+}
+
+#endif
