fd68feb7bc70ece03185f6731eea4e7d89b69ba3
Simon Wallner <me@simonwallner.at>
2012-08-24 10:07:10 +0200

    thought about adding a standalone client app that just wraps a webkit view...

---
+++ client-app/objectif lune/objectif lune/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  objectif lune
+//
+//  Created by Simon Wallner on 22.08.12.
+//  Copyright (c) 2012 Simon Wallner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
